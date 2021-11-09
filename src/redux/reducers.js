f69f81234534902b2490c851e0aa0de0ad6af0de
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-11-09 22:04:28 +0200

    0,8

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,45 +4,27 @@ import { createReducer } from '@reduxjs/toolkit';
 import { submitRecord, changeFilter, deleteRecord } from './actions';
 
 function delRecord(state = {}, action) {
-  switch (action.type) {
-    case 'deleteRecord':
-      const data = state.contacts.filter(elem => elem.id !== action.payload);
-      localStorage.setItem('contacts', JSON.stringify(data));
-      return { ...state, contacts: data };
-
-    default:
-      return state;
-  }
+  const data = state.contacts.filter(elem => elem.id !== action.payload);
+  localStorage.setItem('contacts', JSON.stringify(data));
+  return { ...state, contacts: data };
 }
 
 function addRecord(state = {}, action) {
-  switch (action.type) {
-    case 'submitRecord':
-      if (!state.contacts.find(el => el.name === action.payload[0])) {
-        const data = [
-          ...state.contacts,
-          { name: action.payload[0], number: action.payload[1], id: uuidv4() },
-        ];
-        localStorage.setItem('contacts', JSON.stringify(data));
-        return { ...state, contacts: data };
-      } else {
-        alert(`${action.payload[0]} is already in contacts`);
-        return state;
-      }
-
-    default:
-      return state;
+  if (!state.contacts.find(el => el.name === action.payload[0])) {
+    const data = [
+      ...state.contacts,
+      { name: action.payload[0], number: action.payload[1], id: uuidv4() },
+    ];
+    localStorage.setItem('contacts', JSON.stringify(data));
+    return { ...state, contacts: data };
+  } else {
+    alert(`${action.payload[0]} is already in contacts`);
+    return state;
   }
 }
 
 function filterRecord(state = {}, action) {
-  switch (action.type) {
-    case 'changeFilter':
-      return { ...state, filter: action.payload };
-
-    default:
-      return state;
-  }
+  return { ...state, filter: action.payload };
 }
 
 export const reducer = createReducer(
